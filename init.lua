0d8a521ab61f1530dc6c8c1a66f2e03d211df272
SimplyBlunder <SimplyBlunder@gmail.com>
2023-03-12 17:47:10 +0000

    Added buffer text completion

--- init.lua
+++ init.lua
@@ -6,3 +6,4 @@ require("mothership.plugins.nvim-tree")
 require("mothership.plugins.lualine")
 local async = require "plenary.async" -- used like this because telescope is dependant on it & other method didn't work.
 require("mothership.plugins.telescope")
+require("mothership.plugins.nvim-cmp")
